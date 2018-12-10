5818d66f69e9ed33c632aa15a9b4aede6b27cb2d
Chris Tembreull <ctembreull@skytap.com>
2018-12-10 12:49:38 -0800

    first setup script pass. I suck at bash.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,24 @@
-# placeholder
+# 1. Setup ZSH and make it the default
+echo -e "\nSetting up ZShell (zsh) and making it the default login environment\n"
+sudo apt-get install -qy zsh
+chsh -s $(which zsh)
+
+# 2. Add oh-my-zsh for pure awesomeness
+echo -e "\nAdding robbyrussell/oh-my-zsh toolkit\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# 3. Adding dotfiles
+echo -e "\nReplacing default dotfiles with customized versions\n"
+declare -a files=("hgrc", "zshrc", "zshenv")
+for f in "${files[@]}"; do
+    echo -e "\nChecking ${f}..."
+    if test -f "${HOME}/.${f}"; then
+        echo -e " found. Backing up..."
+        mv "${HOME}/.${f}" "${HOME}/.backup_${f}"
+        echo -e " done."
+    fi
+    echo -e " Replacing..."
+    mv "${f}" "${HOME}/.${f}"
+    echo -e " done."
+done
+echo "\nAll tasks complete. Exiting."
