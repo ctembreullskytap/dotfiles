283ea07bba74ba2f6a4d892a9910c9b600d3521f
Chris Tembreull <ctembreull@skytap.com>
2018-12-10 14:35:49 -0800

    better output escaping

--- setup.sh
+++ setup.sh
@@ -1,30 +1,30 @@
 # 1. Setup ZSH and make it the default
 if ! [ $(test -e "/usr/bin/zsh")  ] ; then
-    echo -e "\nSetting up ZShell (zsh) and making it the default login environment"
+    echo -ne "\nSetting up ZShell (zsh) and making it the default login environment"
     sudo apt-get install -qy zsh
     chsh -s $(which zsh)
 fi
 
 # 2. Add oh-my-zsh for pure awesomeness
 if ! [ $(test -f "${HOME}/.oh-my-zsh") ] ; then
-    echo -e "\nAdding robbyrussell/oh-my-zsh toolkit"
+    echo -ne "\nAdding robbyrussell/oh-my-zsh toolkit"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 # 3. Adding dotfiles
 echo -e "\nReplacing default dotfiles with customized versions:"
-declare -a files=("hgrc", "zshrc", "zshenv" "oh-my-zsh/themes/ctembreull.zsh-theme")
+declare -a files=("hgrc" "zshrc" "zshenv" "oh-my-zsh/themes/ctembreull.zsh-theme")
 for f in "${files[@]}"; do
-    echo -e "\n\tChecking ${f}..."
+    echo -ne "\n\tChecking ${f}..."
     if test -e "${HOME}/.${f}"; then
-        echo -e " found. Backing up..."
+        echo -ne " found. Backing up..."
         mv "${HOME}/.${f}" "${HOME}/.backup_${f}"
-        echo -e " done."
+        echo -ne " done."
     fi
-    echo -e " Replacing..."
+    echo -ne " Replacing..."
     cp "$(dirname -- $0)/${f}" "${HOME}/.${f}"
-    echo -e " done."
+    echo -ne " done."
 done
 
 # also the zsh theme
-echo "\nAll tasks complete. Exiting."
+echo -e "\nAll tasks complete. Exiting."
